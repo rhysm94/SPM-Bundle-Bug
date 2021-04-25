3f0552b81fe4920347cb499aab7871490275fc7a
Rhys Morgan <rhysm@me.com>
2021-04-25 15:05:16 +0100

    Fixes messy indentation in Package.swift

--- Package.swift
+++ Package.swift
@@ -3,17 +3,17 @@
 import PackageDescription
 
 let package = Package(
-    name: "SPM-Bundle-Bug",
+	name: "SPM-Bundle-Bug",
 	defaultLocalization: "en",
 	platforms: [
 		.iOS(.v14),
 		.macOS(.v11),
 	],
-    products: [
+	products: [
 		.library(name: "FooFeature", targets: ["FooFeature"]),
-    ],
-    targets: [
-        .target(name: "Bar"),
+	],
+	targets: [
+		.target(name: "Bar"),
 		.target(name: "FooFeature", dependencies: ["Bar"]),
-    ]
+	]
 )
